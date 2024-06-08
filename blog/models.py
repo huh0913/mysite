9895f4a9f1b94c492c314e99495259f688a351fd
student22 <student22@itpark2.local>
2024-06-08 16:58:19 +0500

    2

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 # Create your models here.
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager): 
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
 
     class Status(models.TextChoices): 
@@ -17,7 +20,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
-
+    objects = models.Manager() # менеджер, применяемый по умолчанию
+    published = PublishedManager() # конкретно-прикладной менеджер
    
 
     class Meta:
