69c5a3b85c6f5194d094186d6a0ed894e23fb9a7
Timur11111111 <newmew@tutamail.com>
2024-06-22 18:10:59 +0500

    add paginations and urls reverse

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager): 
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+    
 class Post(models.Model):
 
     class Status(models.TextChoices): 
@@ -13,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250) 
-    slug = models.SlugField(max_length=250) 
+    slug = models.SlugField(max_length=250, unique_for_date='publish') 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -33,5 +35,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-#j,
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from . views import post_list, post_detail
 
 app_name = 'blog'
 
 urlpatterns = [
     # представления поста
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('', post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
 ]
