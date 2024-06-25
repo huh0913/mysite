989e2c630611b81d2d5ecabc23e4e5c0a5aa96e8
Timur11111111 <newmew@tutamail.com>
2024-06-25 19:36:42 +0500

    admail

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
 
+
+
+class PostListView(ListView):
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     post = Post.published.all()
     paginator = Paginator(post, 2)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     context = {
         'posts': posts,
     }
@@ -23,3 +37,14 @@ def post_detail(request, year, month, day, post):
         'post': post
     }
     return render(request, 'blog/post/detail.html', context)
+
+
+
+def post_share(request, post_id): # Извлечь пост по идентификатору id
+        post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED) 
+        if request.method == 'POST': # Форма была передана на обработку
+            form = EmailPostForm(request.POST)
+            if form.is_valid(): # Поля формы успешно прошли валидацию
+                cd = form.cleaned_data # ... отправить электронное письмо
+        else: form = EmailPostForm() 
+        return render(request, 'blog/post/share.html', { 'post': post, 'form': form })
